b83007524b4858bd2af736fd8ce4b4b44ba31039
vvrublevskyy <vitaly.vrublevskyy@gmail.com>
2019-07-03 17:04:25 +0300

    * Code refactoring

--- src/app/core-modules/services/auth.service.ts
+++ src/app/core-modules/services/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { ApiEndpoint } from '../enums/api-endpoint';
 import * as moment from 'moment';
 import { User } from '../../feature-modules/auth/models';
-import { map, shareReplay, tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { Jwt } from '../enums/jwt';
 import { Observable } from 'rxjs';
 import { JwtResponse } from '../interfaces/jwt-response';
@@ -22,8 +22,12 @@ export class AuthService {
     return moment(expiresAt);
   }
 
+  get token(): string {
+    return localStorage.getItem(Jwt.Token);
+  }
+
   isLoggedIn(): Observable<boolean> {
-    const token = localStorage.getItem(Jwt.Token);
+    const token = this.token;
     return this.http.post<boolean>(ApiEndpoint.Verify, {token})
       .pipe(
         map(data => !!data)
@@ -31,22 +35,22 @@ export class AuthService {
     // TODO: return moment().isBefore(this.expirationTime);
   }
 
-  login(user: User): Observable<{token: string}> {
+  login(user: User): Observable<JwtResponse> {
     return this.http.post<any>(ApiEndpoint.Login, user.serializeLogin())
       .pipe(
         tap(resp => this.setSession(resp)),
       );
   }
 
-  register(user: User): Observable<User> {
-    return this.http.post<User>(ApiEndpoint.Register, user.serialize())
+  register(user: User): Observable<JwtResponse> {
+    return this.http.post<JwtResponse>(ApiEndpoint.Register, user.serialize())
       .pipe(
         tap(resp => this.setSession(resp)),
       );
   }
 
   refreshToken(): Observable<JwtResponse> {
-    const token = localStorage.getItem(Jwt.Token);
+    const token = this.token;
     return this.http.post<JwtResponse>(ApiEndpoint.RefreshToken, {token})
       .pipe(
         tap(resp => this.setSession(resp)),
@@ -62,7 +66,7 @@ export class AuthService {
     localStorage.removeItem(Jwt.Expiration);
   }
 
-  private setSession(resp: any): void {
+  private setSession(resp: JwtResponse): void {
     const expiresAt: moment.Moment = moment().add(resp[Jwt.Expiration], 'second');
     const unixTimestamp: number = expiresAt.valueOf(); // in ms
     localStorage.setItem(Jwt.Token, resp[Jwt.Token]);
