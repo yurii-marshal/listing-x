8bb3345650ac710b1cdeef6e4a0e677c0268c918
vvrublevskyy <vitaly.vrublevskyy@gmail.com>
2019-07-05 13:57:33 +0300

    * Prevent build error

--- src/app/shared-modules/shared.module.ts
+++ src/app/shared-modules/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormValidationErrorComponent } from './components/form-validation-error/form-validation-error.component';
 import { MatButtonModule, MatCheckboxModule, MatDialogModule, MatIconModule } from '@angular/material';
 import { OverlayModule } from '@angular/cdk/overlay';
-import { FileUploaderComponent } from './components/file-uploader/file-uploader.component';
 import { ArrayPipe } from './pipes/array.pipe';
 
 const materialModules = [
@@ -21,7 +20,6 @@ const materialModules = [
   ],
   declarations: [
     FormValidationErrorComponent,
-    FileUploaderComponent,
     ArrayPipe
   ],
   exports: [
