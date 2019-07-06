1b984dfb7d37b31919c91486be9883e4ce873a4c
vvrublevskyy <vitaly.vrublevskyy@gmail.com>
2019-07-06 21:42:28 +0300

    * Working on logout flow

--- src/app/shared-modules/components/base-template/base-template.component.ts
+++ src/app/shared-modules/components/base-template/base-template.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../core-modules/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-base-template',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BaseTemplateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit() {
   }
 
+  logout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/auth/login');
+  }
 }
