65372ac29cf6f36853bba501ef413d90b44f5ab2
vvrublevskyy <vitaly.vrublevskyy@gmail.com>
2019-07-09 20:38:15 +0300

    LIS-45 [FE] Create and update addresses * Working on I'm selling dialog

---
+++ src/app/shared-modules/dialogs/address-dialog/address-dialog.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MatSnackBar } from '@angular/material';
+
+@Component({
+  selector: 'app-address-dialog',
+  templateUrl: './address-dialog.component.html',
+  styleUrls: ['./address-dialog.component.scss']
+})
+export class AddressDialogComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private snackBar: MatSnackBar,
+              private formBuilder: FormBuilder,
+              public dialogRef: MatDialogRef<AddressDialogComponent>,) { }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]]
+    });
+  }
+
+  close() {
+    this.dialogRef.close(); // TODO: model
+  }
+
+}
