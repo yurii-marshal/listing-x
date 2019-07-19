f1b27d3dac6f2558287a608304b2dc3b8c0ab3d4
vvrublevskyy <vitaly.vrublevskyy@gmail.com>
2019-07-19 10:26:32 +0300

    LIS-66 [FE] Offer step 2 * Handled: 'Cannot retrieve offer' by invalid error.

--- src/app/core-modules/resolvers/offer.resolver.ts
+++ src/app/core-modules/resolvers/offer.resolver.ts
@@ -1,24 +1,33 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { LocalStorageKey } from '../enums/local-storage-key';
 import { Offer } from '../models/offer';
-import { switchMap, tap } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { OfferService } from '../core-services/offer.service';
-import { AuthService } from '../core-services/auth.service';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class OfferResolver implements Resolve<Offer> {
 
   constructor(private http: HttpClient,
-              private offerService: OfferService) {
+              private offerService: OfferService,
+              private router: Router,
+              private snackBar: MatSnackBar) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Offer> | Offer {
     const offerId: number = Number(route.queryParams.offerId);
     if (!isNaN(offerId)) {
-      return this.offerService.loadOne(offerId);
+      const config = {duration: 7000, panelClass: 'error-bar'};
+      return this.offerService.loadOne(offerId)
+        .pipe(
+          catchError(err => {
+            this.router.navigate(['../']);
+            this.snackBar.open(`Cannot retrieve offer.`, 'OK', config);
+            return of(null);
+          })
+        );
     }
 
     const data = this.offerService.anonymousOfferData;
