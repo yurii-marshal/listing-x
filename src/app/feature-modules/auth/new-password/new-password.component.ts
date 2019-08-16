618c7c417c28a00b948c9ff98f5b0c7a2205483a
vvrublevskyy <vitaly.vrublevskyy@gmail.com>
2019-08-16 14:01:39 +0300

    LIS-148 reset password malfunction FE

--- src/app/feature-modules/auth/new-password/new-password.component.ts
+++ src/app/feature-modules/auth/new-password/new-password.component.ts
@@ -35,7 +35,7 @@ export class NewPasswordComponent implements OnInit {
     const user = this.form.value;
     this.service.resetPassword(user.password, token)
       .pipe(tap({error: (err: HttpErrorResponse) => {
-          if (err.error.newPassword) {
+          if (err.error.new_password) {
             this.form.get('password').setErrors({passwordused: true});
           } else if (err.error.token) { // invalid token
             this.router.navigateByUrl('/error/expired');
