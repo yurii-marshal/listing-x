cefa4f74c941e575f5be8fa3149fcfc2690ad7c3
vvrublevskyy <vitaly.vrublevskyy@gmail.com>
2019-09-24 20:09:07 +0300

    2. When landing on embeded link, just logout any loggedin user

--- src/app/feature-modules/anonymous/anonymous-offer.resolver.ts
+++ src/app/feature-modules/anonymous/anonymous-offer.resolver.ts
@@ -4,16 +4,19 @@ import { Observable } from 'rxjs';
 import { Offer } from '../../core-modules/models/offer';
 import { OfferService } from '../portal/services/offer.service';
 import { tap } from 'rxjs/operators';
+import { AuthService } from '../../core-modules/core-services/auth.service';
 
 @Injectable()
 export class AnonymousOfferResolver {
 
   constructor(private service: OfferService,
-              private router: Router) {
+              private router: Router,
+              private authService: AuthService) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Offer> | Offer {
     const token = route.params.token;
+    this.authService.logout();
     return this.service.getAnonymousOffer(token)
       .pipe(
         tap({error: err => this.router.navigateByUrl('/error/expired')})
