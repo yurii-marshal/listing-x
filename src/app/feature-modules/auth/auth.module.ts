509827427332ab7b4db3fc4d0e0f0d642fd79013
yurii-marshal <ymarshalofficial@gmail.com>
2020-06-29 15:05:06 +0300

    [LIS 280]: Add mat-select and user role to registration first step

--- src/app/feature-modules/auth/auth.module.ts
+++ src/app/feature-modules/auth/auth.module.ts
@@ -5,14 +5,15 @@ import { RegisterComponent } from './register/register.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule, MatCheckboxModule, MatTabsModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatSelectModule, MatTabsModule } from '@angular/material';
 import { SharedModule } from '../../shared-modules/shared.module';
 import { NewPasswordComponent } from './new-password/new-password.component';
 
 const MatModules = [
   MatCheckboxModule,
   MatButtonModule,
-  MatTabsModule
+  MatTabsModule,
+  MatSelectModule
 ];
 
 @NgModule({
