c3ccf30b3df42ecf7afa57506d577f61c1070f7e
yurii-marshal <ymarshalofficial@gmail.com>
2020-07-08 14:00:20 +0300

    [LIS 331]: PA Step 4, summary init

--- src/app/feature-modules/portal/purchase-agreement/summary/summary.component.ts
+++ src/app/feature-modules/portal/purchase-agreement/summary/summary.component.ts
@@ -1,17 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { OfferService } from '../../services/offer.service';
+import { Offer } from '../../../../core-modules/models/offer';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss']
 })
-export class SummaryComponent implements OnInit {
+export class SummaryComponent implements OnInit, OnDestroy {
+  offer: Offer;
 
-  constructor(private offerService: OfferService) { }
+  private onDestroyed$: Subject<void> = new Subject<void>();
+
+  constructor(
+    private offerService: OfferService,
+    private route: ActivatedRoute,
+  ) {
+  }
 
   ngOnInit() {
     this.offerService.offerProgress = 4;
+
+    this.offerService.getOfferById(+this.route.snapshot.params.id)
+      .pipe(takeUntil(this.onDestroyed$))
+      .subscribe((offer: Offer) => {
+        this.offer = offer;
+      });
+  }
+
+  ngOnDestroy() {
+    this.onDestroyed$.next();
+    this.onDestroyed$.complete();
+  }
+
+  saveOffer() {
   }
 
 }
