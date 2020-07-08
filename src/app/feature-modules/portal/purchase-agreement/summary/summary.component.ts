f171cde4bae0d184c32e850c0be6f354813ae4bc
yurii-marshal <ymarshalofficial@gmail.com>
2020-07-08 18:53:23 +0300

    [LIS 331]: PA Step 4, summary markup completed

--- src/app/feature-modules/portal/purchase-agreement/summary/summary.component.ts
+++ src/app/feature-modules/portal/purchase-agreement/summary/summary.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { OfferService } from '../../services/offer.service';
-import { Offer } from '../../../../core-modules/models/offer';
+import { OfferSummary } from '../../../../core-modules/models/offer';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-summary',
@@ -11,23 +12,24 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./summary.component.scss']
 })
 export class SummaryComponent implements OnInit, OnDestroy {
-  offer: Offer;
+  offerSummary: OfferSummary;
 
   private onDestroyed$: Subject<void> = new Subject<void>();
 
   constructor(
     private offerService: OfferService,
     private route: ActivatedRoute,
+    private snackbar: MatSnackBar,
   ) {
   }
 
   ngOnInit() {
     this.offerService.offerProgress = 4;
 
-    this.offerService.getOfferById(+this.route.snapshot.params.id)
+    this.offerService.loadOfferSummary(+this.route.snapshot.params.id)
       .pipe(takeUntil(this.onDestroyed$))
-      .subscribe((offer: Offer) => {
-        this.offer = offer;
+      .subscribe((data: OfferSummary) => {
+        this.offerSummary = data;
       });
   }
 
@@ -37,6 +39,11 @@ export class SummaryComponent implements OnInit, OnDestroy {
   }
 
   saveOffer() {
+    this.offerService.update(this.offerSummary)
+      .pipe(takeUntil(this.onDestroyed$))
+      .subscribe(() => {
+        this.snackbar.open('Offer is updated');
+      });
   }
 
 }
