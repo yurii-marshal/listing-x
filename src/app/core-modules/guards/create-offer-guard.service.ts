b96b5786d1d4ba151ab8385299089b17aff22902
yurii-marshal <ymarshalofficial@gmail.com>
2020-07-16 11:02:50 +0300

    [LIS 306] PA Step 2 Page 7 fix create PA permission

--- src/app/core-modules/guards/create-offer-guard.service.ts
+++ src/app/core-modules/guards/create-offer-guard.service.ts
@@ -10,11 +10,13 @@ import {
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { OfferService } from '../../feature-modules/portal/services/offer.service';
+import { AuthService } from '../core-services/auth.service';
 
 @Injectable()
 export class CreateOfferGuardService implements CanActivate, CanActivateChild {
 
   constructor(private offerService: OfferService,
+              private authService: AuthService,
               private router: Router) {
   }
 
@@ -22,8 +24,7 @@ export class CreateOfferGuardService implements CanActivate, CanActivateChild {
     const redirectUrl = '/portal/purchase-agreement/step-one';
     return this.offerService.getOfferById(+route.params.id)
       .pipe(
-        // TODO: check on step allowing
-        map(() => true),
+        map(() => this.authService.currentUser.accountType === 'agent'),
         catchError(() => of(this.router.parseUrl(redirectUrl)))
       );
   }
