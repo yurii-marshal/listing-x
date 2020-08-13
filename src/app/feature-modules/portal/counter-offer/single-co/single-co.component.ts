2386e6aa974d4dda60682e827c9fb86d1186f582
yurii-marshal <ymarshalofficial@gmail.com>
2020-08-13 17:08:17 +0300

    add CO files

---
+++ src/app/feature-modules/portal/counter-offer/single-co/single-co.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-co',
+  templateUrl: './single-co.component.html',
+  styleUrls: ['./single-co.component.scss']
+})
+export class SingleCOComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
